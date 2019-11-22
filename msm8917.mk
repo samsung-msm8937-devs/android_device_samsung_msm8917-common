b8f076c6bd5bd3b3aecedf67adb2d08b886e53b3
Mikhail Naganov <mnaganov@google.com>
2019-11-22 10:56:29 -0800

    msm8917-common: Switch to Audio HAL V6

--- msm8917.mk
+++ msm8917.mk
@@ -49,13 +49,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio.service \
-    android.hardware.audio@5.0 \
-    android.hardware.audio@5.0-impl \
+    android.hardware.audio@6.0-impl \
     android.hardware.audio.common@4.0 \
     android.hardware.audio.common@4.0-util \
     android.hardware.audio.effect@2.0-impl \
-    android.hardware.audio.effect@5.0 \
-    android.hardware.audio.effect@5.0-impl \
+    android.hardware.audio.effect@6.0-impl \
     android.hardware.soundtrigger@2.2-impl
 	
 PRODUCT_PACKAGES += \
